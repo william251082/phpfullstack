c7a322946c55f040e4cf3500750261493c3d8615
william <william.del.rosario@iceshop.nl>
2020-04-27 12:17:54 +0200

    output escaping

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title>Home</title>
+        <title>Output escaping</title>
+        <meta charset="utf-8">
     </head>
     <body>
-        <h1>Welcome</h1>
-            <p>Hello from View!</p>
+        <h1>Output escaping</h1>
+            <?php
+
+            // Display the results of submitting the form
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                echo "Hello, " . htmlspecialchars($_POST['name']);
+            }
+
+            ?>
+
+        <form method="post">
+            <div>
+                <label for="name">Your name</label>
+                <input id="name" name="name" autofocus />
+            </div>
+
+            <div>
+                <button type="submit">Submit</button>
+            </div>
+        </form>
     </body>
 </html>
