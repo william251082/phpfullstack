eddab2f4d36f31c0abfbcaef3e89a4553cc9a4e7
william <william.del.rosario@iceshop.nl>
2020-04-27 09:48:15 +0200

    get controller action form url with a variable structure

--- core/Router.php
+++ core/Router.php
@@ -11,25 +11,33 @@ class Router
     protected $routes = [];
     protected $params = [];
 
-    public function add($route, $params): void
+    public function add($route, $params=[]): void
     {
+        // Convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//', '\\/', $route);
+
+        // Convert variables e.g. {controller}
+        $route = preg_replace('/\{([a-z])\}/', '(?P</1>[a-z]+)', $route);
+
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^' . $route . '$/i';
+
         $this->routes[$route] = $params;
     }
 
     public function match($url): bool
     {
-        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
-
-        if (preg_match($reg_exp, $url, $matches)) {
-            $params = [];
-            foreach ($matches as $key => $match) {
-                if (is_string($key)) {
-                    $params[$key] = $match;
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
                 }
-            }
-            $this->params = $params;
+                $this->params = $params;
 
-            return true;
+                return true;
+            }
         }
 
         return false;
--- public/index.php
+++ public/index.php
@@ -7,13 +7,11 @@ $router = new Router();
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 $router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
 
-$url = $_SERVER['QUERY_STRING'];
+echo '<pre>';
+//	var_dump($router->getRoutes());
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '<pre>';
 
-if($router->match($url)) {
-    echo '<pre>';
-    var_dump($router->getParams());
-    echo '</pre>';
-} else {
-    echo "No route found for URL '$url'";
-}
