8137982ed643815b260cc83ef4c24044ae789758
william <william.del.rosario@iceshop.nl>
2020-04-27 09:16:59 +0200

    created and required router class

---
+++ core/Router.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iceshop
+ * Date: 27-4-20
+ * Time: 9:12
+ */
+
+class Router
+{
+
+}
--- public/index.php
+++ public/index.php
@@ -1,2 +1,8 @@
 <?php
-echo 'hi';
+echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"' . '<br>';
+
+require '../core/Router.php';
+
+$router = new Router();
+
+echo 'Class = ' . get_class($router);
