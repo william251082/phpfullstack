912fa896f0a2261d720b516dd77875cdc988924a
william <william.del.rosario@iceshop.nl>
2020-04-27 09:53:54 +0200

    get controller variables of any format to url

--- public/index.php
+++ public/index.php
@@ -8,10 +8,19 @@ $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 $router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
 $router->add('{controller}/{action}');
-$router->add('admin/{action}/{controller}');
+$router->add('{controller}/{id:\d+}/{action}');
 
 echo '<pre>';
-//	var_dump($router->getRoutes());
 echo htmlspecialchars(print_r($router->getRoutes(), true));
 echo '<pre>';
 
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for URL '$url'";
+}
